d9b2f8efdc42fe32e1a9926d6b02149efc13380d
Administrator <Administrator@andro02>
2016-03-30 15:53:01 +0200

    chicles2

--- src/chiclee/Chiclee.java
+++ src/chiclee/Chiclee.java
@@ -5,25 +5,34 @@ public class Chiclee {
     public void ofertaChicles(int envoltorio1, int envoltorio2, int chiclesComprados) {
         int devolucion;
         int devolucion2;
-        if (envoltorio2 < envoltorio1) {
-            devolucion = chiclesComprados / envoltorio1;
-            if (devolucion <= envoltorio2) {
-                devolucion2 = devolucion / envoltorio1;
-                System.out.println(devolucion);
-            } else {
-                devolucion2 = devolucion / envoltorio1;
-                System.out.println(devolucion / envoltorio1);
-            }
-
-            System.out.println(chiclesComprados + devolucion + devolucion2);
+        int acumulacionDevoluciones;
+    if(envoltorio2 < envoltorio1){
+        devolucion = chiclesComprados / envoltorio1;
+        if(devolucion<=envoltorio2){
+            devolucion2 = devolucion / envoltorio1;  
+            System.out.println(devolucion);
+            acumulacionDevoluciones = devolucion2;
         } else {
-            System.out.println("RUINA");
+            
+        devolucion2 = devolucion / envoltorio1; 
+        acumulacionDevoluciones = devolucion2;
+        while(devolucion2 > envoltorio1){
+            devolucion2 = devolucion2 / envoltorio1; 
+            acumulacionDevoluciones = acumulacionDevoluciones + devolucion2;
         }
+            
+        System.out.println(devolucion2%envoltorio1);
+        }
+        
+        System.out.println(chiclesComprados+devolucion+acumulacionDevoluciones);
+    } else {
+        System.out.println("RUINA");
     }
-
-    public Chiclee() {
+    
+}
+    public Chiclee(){
         ofertaChicles(5, 1, 25);
-    }
+}
 
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
